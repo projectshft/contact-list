654cd7c60a42c8ac03022bbc4004d8dda42dd3ee
abeakaid <74392388+abeK021@users.noreply.github.com>
2021-04-16 10:52:07 -0400

    Initial Start Testing

---
+++ src/components/InputContact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+const TestRender = () => {
+  return (
+    <h4>test input contact</h4>
+  )
+}
+
+export default TestRender;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import { BrowserRouter, Switch, Route, Link, useHistory } from 'react-router-dom';
+import TestRender from './components/InputContact'
+
+
+const App = () => (
+    <div>
+  <Main />
+  </div> 
+)
+
+const Main = () => (
+  <main>
+    <Switch>
+      <Route exact path="/" component={ContactsViewHeader}></Route>
+      <Route path="/contact-form" component={TestRender}></Route>
+    </Switch>
+  </main>
+)
+
+const ContactsViewHeader = () => {
+  return (
+    <div className="heading"> 
+    <h1 className="heading-title">Contacts</h1>
+    <hr></hr>
+    <button className="btn btn-primary">Add Contact</button>
+    </div>
+  )
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
