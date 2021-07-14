e856941cdda8e984e5bc8d5e4a0a0ef759ef321d
Kris Yerkie <kyerkie@myncca.com>
2021-07-14 15:16:36 -0400

    Updated file - ContactDetail not rendering

--- src/components/ContactDetail.js
+++ src/components/ContactDetail.js
@@ -1,23 +1,20 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import React from "react";
+
 const ContactDetail = ({ contactID, contacts }) => {
 
-  const contact = contacts.find((c) => {
-    return c.contactID.toString() === contactID;
-  });
-  console.log(contact);
+  const contact = contacts.find((c) => c.contactID.toString() === contactID);
 
   return (
     <div className="container-fluid">
       <div className="row">
-        <div className="col-md-6 offset-md-3">
+        <div className="col-md-4 offset-md-3">
           <div className="text-center">
-            <div className="contact-item">
-              <h1 className="text-center">Contact Detail</h1>
-              <h2 className="text-center">{contact.name}</h2>
+            <h1>Contact Detail</h1>
+              <h2>{contact.name}</h2>
               <img src={contact.imageURL} alt={contact.name} />
-              <h5 className="text-center">{contact.email}</h5>
-              <h3 className="text-center">{contact.phone}</h3>
+              <h5>{contact.email}</h5>
+              <h3>{contact.phone}</h3>
               <Link to="/" className="btn btn-primary btn">
                 Return to Contact List
               </Link>
@@ -25,7 +22,7 @@ const ContactDetail = ({ contactID, contacts }) => {
           </div>
         </div>
       </div>
-    </div>
   );
 };
+
 export default ContactDetail;
