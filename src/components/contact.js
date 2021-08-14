bf69d631ddf506b8abae9c2f7f54c897d548e665
Ryan Passer <rpasser93@gmail.com>
2021-08-14 15:03:37 -0400

    Set up state for index and contact

--- src/components/contact.js
+++ src/components/contact.js
@@ -12,22 +12,25 @@ const testData = {
   ]
 };
 
-const Contact = () => (
-  <div>
-    <div className="container">
-      <div className="row">
-        <div className="col-md-4 offset-md-4">
-          <div className="col border">
-            <img src={testData.contacts[0].image_url} className="selected-prof-pic rounded mx-auto d-block" alt=""></img>
-            <h4 className="text-center">{testData.contacts[0].name}</h4>
-            <p className="text-center fs-5 selected-p">{testData.contacts[0].email}</p>
-            <p className="text-center fs-5 selected-p">{testData.contacts[0].phone_number}</p>
+const Contact = () => {
+
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4 offset-md-4">
+            <div className="col border">
+              <img src={testData.contacts[0].image_url} className="selected-prof-pic rounded mx-auto d-block" alt=""></img>
+              <h4 className="text-center">{testData.contacts[0].name}</h4>
+              <p className="text-center fs-5 selected-p">{testData.contacts[0].email}</p>
+              <p className="text-center fs-5 selected-p">{testData.contacts[0].phone_number}</p>
+            </div>
+            <Link to="/" type="button" className="btn btn-warning">Back</Link>
           </div>
-          <Link to="/" type="button" className="btn btn-warning">Back</Link>
         </div>
       </div>
     </div>
-  </div>
-);
+  )
+};
 
 export default Contact
--- src/components/contact_new.js
+++ src/components/contact_new.js
@@ -1,23 +1,44 @@
+import React, {useState} from 'react';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
-const ContactNew = () => (
-  <div>
-    <div className="container">
-      <div className= "row">
-        <div className= "col-md-6 offset-md-3">
-        <label className="form-label">Full Name</label>
-          <input type="text" className="form-control" id="nameInput" placeholder="Enter Name"></input>
-          <label className="form-label">Email Address</label>
-          <input type="text" className="form-control" id="emailInput" placeholder="Enter Email"></input>
-          <label className="form-label">Phone Number</label>
-          <input type="text" className="form-control" id="phoneInput" placeholder="Enter Phone"></input>
-          <label className="form-label">Image URL</label>
-          <input type="text" className="form-control" id="imageInput" placeholder="Enter URL"></input>
-          <Link to="/" type="button" className="btn btn-primary">Add Contact</Link>
+const ContactNew = (props) => {
+
+  const [state, setState] = useState ({
+    profPic: '',
+    name: '',
+    email: '',
+    phoneNum: ''
+  });
+
+  const changeHandler = (e) => {
+    setState({...state, [e.target.id]: e.target.value});
+  };
+  
+  return (
+    <div>
+      <div className="container">
+        <div className= "row">
+          <div className= "col-md-6 offset-md-3">
+
+          <label className="form-label">Full Name</label>
+            <input type="text" className="form-control" id="name" placeholder="Enter Name" onChange={changeHandler}></input>
+
+            <label className="form-label">Email Address</label>
+            <input type="text" className="form-control" id="email" placeholder="Enter Email" onChange={changeHandler}></input>
+
+            <label className="form-label">Phone Number</label>
+            <input type="text" className="form-control" id="phoneNum" placeholder="Enter Phone" onChange={changeHandler}></input>
+
+            <label className="form-label">Image URL</label>
+            <input type="text" className="form-control" id="profPic" placeholder="Enter URL" onChange={changeHandler}></input>
+
+            <Link to="/" type="button" className="btn btn-primary" onClick={props.clickHandler}>Add Contact</Link>
+          </div>
+
         </div>
       </div>
     </div>
-  </div>
-);
+  )
+};
 
 export default ContactNew
