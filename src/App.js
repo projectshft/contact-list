64d5514c823b3263a3832b1b9d2225056c649755
Philip Krynicki <philipkrynicki@Philips-MacBook-Air.local>
2021-08-13 22:42:49 -0500

    Routes added and contactList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Contacts from './components/contactList';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+
+const Main = () => (
+  <Switch>
+    <Route exact path="/" component={Contacts} />
+    {/* //  need to change component below */}
+    <Route path="/compiledContacts" component={Contacts} />
+    
+  </Switch>
+)
 
 function App() {
   return (
-    
-        <Contacts />
+        <BrowserRouter>
+        <Main />
+        </BrowserRouter>
         
   )   
 }
--- src/components/contactList.js
+++ src/components/contactList.js
@@ -1,35 +1,75 @@
-import React, { Component } from 'react';
 
-class ContactList extends Component {
-  constructor () {
-    super()
-  }
-  render() {
-    return (
-      <><h1>Contact List</h1>
-{/* /* //  Full Name
-//  Email address
-//  Phone Number
-//  Image URL
-//  Add Contact blue button */ }
+//  functional component below
+import React, { Component, useState } from 'react';
+const ContactList = () => {
+  const [fullName, setFullName] = useState('');
+  const [emailAddress, setEmailAddress] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const nameEventHandler = (e) => {setFullName(e.target.value)}
+  const emailEventHandler = (e) => {setEmailAddress(e.target.value)}
+  const phoneEventHandler = (e) => {setPhoneNumber(e.target.value)}
+  const imageEventHandler = (e) => {setImageUrl(e.target.value)}
+  
+
+  return(
+  <>
+  <h1>Contact List</h1>
       <div>
-      <input type="text" placeholder="Full Name" />
+        <input type="text" placeholder="Full Name" value={fullName} onChange={nameEventHandler}/>
+        </div>
+        <div>
+        <input type="text"  placeholder="Email address" value={emailAddress} onChange={emailEventHandler}/>
+        </div>
+        <div>
+        <input type="text"  placeholder="Phone Number" value={phoneNumber} onChange={phoneEventHandler} />
+        </div>
+        <div>
+        <input type="text"  placeholder="Image URL" value={imageUrl} onChange={imageEventHandler}/>
+        <hr></hr>
+        <input type="submit"></input>
       </div>
-      <div>
-      <input type="text"  placeholder="Email address" />
-      </div>
-      <div>
-      <input type="text"  placeholder="Phone Number" />
-      </div>
-      <div>
-      <input type="text"  placeholder="Image URL" />
-      </div></>
+  </>
+  );
+}
+
+
+
+// class ContactList extends Component {
+//   constructor () {
+//     super()
+//   }
+
+
+
+//   render() {
+//     return (
+//       <><h1>Contact List</h1>
+// {/* /* //  Full Name
+// //  Email address
+// //  Phone Number
+// //  Image URL
+// //  Add Contact blue button */ }
+//       <div>
+//       <input type="text" placeholder="Full Name" />
+//       </div>
+//       <div>
+//       <input type="text"  placeholder="Email address" />
+//       </div>
+//       <div>
+//       <input type="text"  placeholder="Phone Number" />
+//       </div>
+//       <div>
+//       <input type="text"  placeholder="Image URL" />
+//       <hr></hr>
+//       <input type="submit"></input>
+//       </div></>
       
       
     
 
-    )
-  }
-}
+//     )
+//   }
+// }
 
 export default ContactList;
