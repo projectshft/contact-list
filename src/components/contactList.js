8360b49532330785ae435e3935e02662236875ec
Philip Krynicki <philipkrynicki@Philips-MacBook-Air.local>
2021-08-18 16:11:56 -0500

    Cleaned up code formatting

--- src/components/contactList.js
+++ src/components/contactList.js
@@ -4,22 +4,21 @@ import React, { Component, useState } from 'react';
 const ContactList = (props) => {
  
 //  content that will show up on the DOM 
-  return(
-  <>
-    
+return(
+  <>  
     { 
       props.contacts.length === 0
       ? <p>No Contacts</p> : <p>Contacts List</p>
     }
-    <ol>
-      {
+     <ol>
+       {
         props.contacts.map((contact) => {
-          return (<li key={contact.phoneNumber}><p>Name: {contact.fullName}</p> <p>E-mail address: {contact.emailAddress}</p> <p>Phone number: {contact.phoneNumber}</p> <img src={contact.imageUrl}/></li>
+        return (<li key={contact.phoneNumber}><p>Name: {contact.fullName}</p> <p>E-mail address: {contact.emailAddress}</p> <p>Phone number: {contact.phoneNumber}</p> <img src={contact.imageUrl}/></li>
         )})
-      }
+       }
     </ol>
   </>
-  );
+ );
 }
 
 //  export component to App.js
