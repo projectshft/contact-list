177c9a1d0ac284d43dc0d77ca96dc32ac472300c
Jason Stover <jstover8807@gmail.com>
2021-10-08 17:31:51 -0400

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ContactList from './components/contact_list';
+import AddContact from './components/add_contact';
+import PropTypes from 'prop-types';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => (
+  <div>
+    <Main />
+  </div>
+);
+
+const ContactAPI = {
+  contacts: [
+    { pic: 'https://www.looper.com/img/gallery/the-entire-history-of-dunes-house-atreides/intro-1602509817.jpg', name: 'Paul Atreides', email: 'muaddib@gmail.com', phone: '5555555' },
+    { pic: 'https://dazedimg-dazedgroup.netdna-ssl.com/900/azure/dazed-prod/1310/1/1311717.jpg', name: 'Chani', email: 'fremen#1@gmail.com', phone: '2222222'}
+  ],
+  all: function() {
+    return this.contacts;
+  },
+  get: function(id) {
+    const isContact = p => p.number === id;  //Confused as to how this is working.  Need ID in API
+    return this.contacts.find(isContact);
+  }
+};
+
+const Index = () => {
+  return (
+  <div className="ContactList">
+    <ContactList />
+  </div>
+  );
+}
+
+const AddCon = () => {
+  return (
+    <div className="AddContact">
+      <AddContact />
+    </div>
+  );
+}
+
+const Contact = () => <h1>Contact</h1>;
+
+
+
+const Main = () => (
+  <main>
+    <Switch>
+      <Route exact path="/" component={Index} />
+      <Route path="/contacts/:number" component={Contact} />   
+      <Route path="/addcontact" component={AddCon} />   
+    </Switch>
+  </main>
+);
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
