05975f8325ebdd1422a3bdf78b9cf7fc6dc5b129
Jason Stover <jstover8807@gmail.com>
2021-10-11 18:03:50 -0400

    Clickable row and route working.  Not rendering

--- src/index.js
+++ src/index.js
@@ -13,38 +13,17 @@ const App = () => (
 
 const ContactAPI = {
   contacts: [
-    { id: 1, image_url: 'https://www.looper.com/img/gallery/the-entire-history-of-dunes-house-atreides/intro-1602509817.jpg', name: 'Paul Atreides', email: 'muaddib@gmail.com', phone_number: '5555555' },
-    { id: 2, image_url: 'https://dazedimg-dazedgroup.netdna-ssl.com/900/azure/dazed-prod/1310/1/1311717.jpg', name: 'Chani', email: 'fremen#1@gmail.com', phone_number: '2222222'}
+    { id: 0, image_url: 'https://www.looper.com/img/gallery/the-entire-history-of-dunes-house-atreides/intro-1602509817.jpg', name: 'Paul Atreides', email: 'muaddib@gmail.com', phone_number: '5555555' },
+    { id: 1, image_url: 'https://dazedimg-dazedgroup.netdna-ssl.com/900/azure/dazed-prod/1310/1/1311717.jpg', name: 'Chani', email: 'fremen#1@gmail.com', phone_number: '2222222'}
   ], 
   all: function() {
     return this.contacts;
   },
-  get: function(id) {
-    const isContact = p => p.number === id;  //Confused as to how this is working.  Need ID in API
-    return this.contacts.find(isContact);
+  get: function(key) {
+    const isContact = p => p.id === key;  
+    return this.contacts.find(isContact);  
   }
 };
-
-
-
-
-const AddCon = () => {
-  return (
-    <div className="add-contact">
-      <AddContact />
-    </div>
-  );
-}
-
-const Contact = () => {
-  return (
-    <div className="selected-contact">
-      <SelectedContact />
-    </div>
-  );
-};
-
-
 
 const Main = () => (
   <main>
@@ -55,6 +34,44 @@ const Main = () => (
     </Switch>
   </main>
 );
+
+const Index = () => {
+  return (
+    <section>
+    <Link to="/addcontact">Add Contact</Link>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col" id="view-link">Profile Link</th>
+                <th scope="col" id="pic-column">Profile Pic</th>
+                <th scope="col" id="name-column">Name</th>
+                <th scope="col" id="email-column">Email</th>
+                <th scope="col" id="phone-column">Phone Number</th>
+            </tr>
+        </thead> 
+        
+          <ContactList /> 
+                     
+    </table> 
+  </section>  
+  );
+}
+
+const Contact = (key) => {  
+  return (
+    <div className="selected-contact">
+      <SelectedContact />
+    </div>
+  );
+};
+
+const AddCon = () => {
+  return (
+    <div className="add-contact">
+      <AddContact />
+    </div>
+  );
+}
 
 const AddContact = props => {     
   return (        
@@ -74,49 +91,36 @@ const AddContact = props => {
   )
 };
 
-const Index = () => {
+const ContactList = () => {     
   return (
-    <section>
-    <Link to="/addcontact">Add Contact</Link>
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col" id="pic-column">Profile Pic</th>
-                <th scope="col" id="name-column">Name</th>
-                <th scope="col" id="email-column">Email</th>
-                <th scope="col" id="phone-column">Phone Number</th>
-            </tr>
-        </thead> 
-        <tbody>
-          <ContactList /> 
-        </tbody>             
-    </table> 
-  </section>  
-  );
-}
-
-
-const ContactList = props => {     
-  const contactsArray = ContactAPI.contacts;
-  const rows = [];
-
-  for(let i = 0; i < contactsArray.length; i++) {
-    rows.push(
-      <tr>          
-          <td><img src={contactsArray[i].image_url}></img></td>
-          <td>{contactsArray[i].name}</td>
-          <td>{contactsArray[i].email}</td>
-          <td>{contactsArray[i].phone_number}</td>
-      </tr>     
-    )
-  }
-  console.log(rows);
-  return rows;
+  <tbody>
+  {ContactAPI.all().map(rows => (  //Using map will loop through the entire array and display all inherently?
+    <Link to={`/contacts/${rows.id}`} key={rows.id} className="table-row">               
+          <td><img src={rows.image_url}></img></td>
+          <td>{rows.name}</td>
+          <td>{rows.email}</td>
+          <td>{rows.phone_number}</td>
+      </Link> 
+  ))}
+  </tbody>
+  )
   
 };
 
 const SelectedContact = props => {
-  return <div>TODO</div>;
+  const contact = ContactAPI.get(parseInt(props.match.params.id, 10));  
+  //const player = PlayerAPI.get(parseInt(props.match.params.number, 10));  code from Module example
+
+  if (!contact) {
+    return <div>Contact does not exist</div>;
+  }  
+  return (
+    <section>
+      <h1>Selected Contact</h1>
+      <div>TODO</div>
+      <div></div>
+    </section>
+  )
 };
 
 
@@ -129,5 +133,25 @@ ReactDOM.render(
 );
 
 /*
+const contactsArray = ContactAPI.contacts;
+  const rows = [];  
+  
+
+  for(let i = 0; i < contactsArray.length; i++) {
+    rows.push(
+      <tr key={i}>                  
+          <td><Link to={'/contacts/${i}'}>View Profile</Link></td>
+          <td><img src={contactsArray[i].image_url}></img></td>
+          <td>{contactsArray[i].name}</td>
+          <td>{contactsArray[i].email}</td>
+          <td>{contactsArray[i].phone_number}</td>
+      </tr> 
+        
+    )
+  }  
+  return rows;
+
+
+  <td><Link to={'/contacts/${rows.id}'}>View Profile</Link></td>
 
   */
