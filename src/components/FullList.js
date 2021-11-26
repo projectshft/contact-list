1d842c453e4661db16293dd7f79e0bb2e1382a1d
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2021-11-26 13:25:46 -0500

    begginning to build out FullList

--- src/components/FullList.js
+++ src/components/FullList.js
@@ -2,7 +2,10 @@
 import { Link } from 'react-router-dom';
 
 const FullList = () => (
-  <Link to="/add-contacts">Add Contact</Link>
+  <div>
+    <Link to="/add-contacts">Add Contact</Link>
+    <h3>List goes under here</h3>
+  </div>
 )
 
 export default FullList
