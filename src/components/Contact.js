bf200d738c5e34d0fb14875abbba46e9ee97ecbb
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2021-11-26 18:38:40 -0500

    images can be submitted but their sizing needs to be limited

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ const Contact = ({post}) => {
   return (
     <div>
       <li>
-        <span>{post.img.img} – {post.name.name} – {post.email.email} – {post.phone.phone}</span>
+        <span><img className="profile-img" src={post.img.img} /> – {post.name.name} – {post.email.email} – {post.phone.phone}</span>
       </li>
     </div>
   )
