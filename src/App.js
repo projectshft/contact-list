a04593a14ae15eeba1e8e3a7dad36c858824b81f
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2021-11-26 11:45:56 -0500

    installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AddContacts from './components/AddContacts';
 
-const App = () => (
-  <div>
+const App = () => {
+  return (
+    <div>
     <h1>Contact List</h1>
     <AddContacts />
   </div>
-)
+  )
+}
 
 export default App;
