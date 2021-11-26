01194f263409ca10e8d43d18233ddfcd7c57ef1e
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2021-11-26 11:25:23 -0500

    handleNameChange passes console log test

--- src/components/AddContacts.js
+++ src/components/AddContacts.js
@@ -1,11 +1,21 @@
 
-const AddContacts = () => (
+import React, { useState } from 'react';
+
+const AddContacts = () => {
+
+const [name, setName] = useState('Josh')
+
+const handleNameChange = () => {
+  console.log('test handleNameChange')
+}
+
+return (
   <div>
     <h3>Add information below:</h3>
     <form>
       <span>Image URL: <input /></span>
       <br></br>
-      <span>Name: <input /></span>
+      <span>Name: <input value={name} onChange={handleNameChange}/></span>
       <br></br>
       <span>Email: <input /></span>
       <br></br>
@@ -13,7 +23,7 @@ const AddContacts = () => (
       <br></br>
     </form>
   </div>
-
 )
+}
 
 export default AddContacts
