9c46bfaf2bd5a592a2aa7e8b72af7a824c253723
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2021-11-26 14:43:03 -0500

    the tester function in FullList.js is successfully console logging the state of Main.js

--- src/components/FullList.js
+++ src/components/FullList.js
@@ -2,21 +2,16 @@
 import { Link } from 'react-router-dom';
 import Contact from './Contact';
 
-const FullList = (p) => {
+const FullList = (prop) => {
   
-  const postContacts = p.contacts.map((post, index) => {
-    return (
-      <Contact key={index} post={post} />
-    )
-  })
-  
+  const tester = () => {
+    console.log(prop)
+  }
   return (
     <div>
       <Link to="/add-contacts">Add Contact</Link>
       <h3>List goes under here</h3>
-      <ul>
-        {postContacts}
-      </ul>
+      <button onClick={tester}>Test</button>
     </div>
     
   )
