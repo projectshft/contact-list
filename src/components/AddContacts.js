90b6ccd662229dc5015e77213cd88f62e3055d31
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2021-11-26 13:22:46 -0500

    replaced filler texts in inputs for placeholders

--- src/components/AddContacts.js
+++ src/components/AddContacts.js
@@ -5,22 +5,22 @@ import React, { useState } from 'react';
 
 const AddContacts = (props) => {
 
-  const [img, setImg] = useState('Josh\'s face')
+  const [img, setImg] = useState('')
   const handleImgChange = (e) => {
     setImg(e.target.value)
   }
 
-  const [name, setName] = useState('Josh')
+  const [name, setName] = useState('')
   const handleNameChange = (e) => {
   setName(e.target.value)
   }
 
-  const [email, setEmail] = useState('me@me.com')
+  const [email, setEmail] = useState('')
   const handleEmailChange = (e) => {
   setEmail(e.target.value)
   }
 
-  const [phone, setPhone] = useState('1234')
+  const [phone, setPhone] = useState('')
   const handlePhoneChange = (e) => {
   setPhone(e.target.value)
   }
@@ -41,13 +41,13 @@ return (
     <Link to="/">Back</Link>
     <h3>Add information below:</h3>
     <form>
-      <span>Image URL: <input value={img} onChange={handleImgChange}/></span>
+      <span>Image URL: <input value={img} onChange={handleImgChange} placeholder="URL link"/></span>
       <br></br>
-      <span>Name: <input value={name} onChange={handleNameChange}/></span>
+      <span>Name: <input value={name} onChange={handleNameChange} placeholder="First and Last"/></span>
       <br></br>
-      <span>Email: <input value={email} onChange={handleEmailChange}/></span>
+      <span>Email: <input value={email} onChange={handleEmailChange} placeholder="e.g. me@webaddress.org"/></span>
       <br></br>
-      <span>Phone Number: <input value={phone} onChange={handlePhoneChange}/></span>
+      <span>Phone Number: <input value={phone} onChange={handlePhoneChange} placeholder="e.g. (xxx) xxx-xxxx"/></span>
       <br></br>
       <button type="button" className="btn btn-primary" onClick={passState}>Submit</button>
     </form>
