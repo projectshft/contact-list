44f1b1f854a00f976ed20424dfc5b922744bb34b
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2021-11-26 14:54:06 -0500

    full list posts <li> in Contact.js

--- src/components/FullList.js
+++ src/components/FullList.js
@@ -4,14 +4,25 @@ import Contact from './Contact';
 
 const FullList = (prop) => {
   
-  const tester = () => {
-    console.log(prop)
-  }
+  // const tester = () => {
+  //   console.log(prop.contacts)
+  // }
+
+  const postContacts = prop.contacts.map((post, index) => {
+    return (
+      <Contact key={index} post={post}/>
+    )
+  })
+
+
   return (
     <div>
       <Link to="/add-contacts">Add Contact</Link>
       <h3>List goes under here</h3>
-      <button onClick={tester}>Test</button>
+      <ul>
+        {postContacts}
+      </ul>
+      {/* <button onClick={tester}>Test</button> */}
     </div>
     
   )
