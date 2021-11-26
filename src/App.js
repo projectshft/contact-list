fa8abd3ff5f141c0f5ad436b462b74732c109629
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2021-11-26 11:13:22 -0500

    created AddContacts.js and linked it to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import AddContacts from './components/AddContacts';
 
 const App = () => (
   <div>
     <h1>test</h1>
+    <AddContacts />
   </div>
 )
 
---
+++ src/components/AddContacts.js
@@ -0,0 +1,6 @@
+
+const AddContacts = () => (
+  <h1>testing</h1>
+)
+
+export default AddContacts
