7c9ff9c36af9c2158b464ef7cb6ec0ece0a34f70
j-d-hunt <jdhunt@Jordans-MBP.lan>
2021-12-01 09:44:38 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container-fluid justify-content-md-center">
+      <br/>
+      <div class="row">
+        <div class="col">
+          <h1>Contact List</h1>
+        </div>
+      </div>
+      <br/>
+      <div class="row">
+        <div class="col">
+          <button type="button" class="btn btn-primary">
+          Add Contact
+          </button>
+        </div>
+      </div>
+      <br/>
+      <div class="row" id="main-grid">
+        <div class="col-sm"><h6>Profile Pic</h6></div>
+        <div class="col-sm"><h6>Name</h6></div>
+        <div class="col-sm"><h6>Email Address</h6></div>
+        <div class="col-sm"><h6>Phone Number</h6></div>
+      </div>
     </div>
   );
 }
