fab61bcaea0a391aad59a43b57ef686a69da0bdd
Ian Holmes <iqholmes@gmail.com>
2022-02-18 14:55:09 -0500

    initial commit. basic setup

---
+++ src/ContactInfo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ContactInfo = () => (
+  <div>
+    <h1>Contact Info</h1>
+  </div>
+);
+
+export default ContactInfo;
---
+++ src/NewContact.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NewContact = () => (
+  <div>
+    <h1>Add New Contact</h1>
+  </div>
+);
+
+export default NewContact;
