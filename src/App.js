f88c21eec9ce1da1d3905fda8af9069fb4c7f4bc
sbistransin <48774173+sbistransin@users.noreply.github.com>
2022-02-18 17:23:34 -0500

    add bones of new contact form

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './App.css';
 import Contacts from './Contacts';
 
-const Header = () => <div>Contact List</div>;
+const Header = () => (
+  <h1 className="text-center">Contact List</h1>
+);
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Contacts /> 
+    <div className="container">
+      <div className="col-md-5 offset-md-3">
+        <div className="row">
+          <Header />
+          <Contacts /> 
+        </div> 
+      </div>
     </div>
   );
 }
--- src/ContactNew.js
+++ src/ContactNew.js
@@ -2,7 +2,29 @@ import React from 'react';
 
 const ContactNew = () => {
   return (
-    <div>New Contact</div>
+    <form>
+      <div class="form-group">
+        <label for="nameInput">Full Name</label>
+        <input type="text" className="form-control" id="nameInput" placeholder="John Doe" />
+      </div>
+      <br/>
+      <div class="form-group">
+        <label for="emailInput">Email</label>
+        <input type="email" className="form-control" id="emailInput" placeholder="John@email.com" />
+      </div>
+      <br/>
+      <div class="form-group">
+        <label for="phoneInput">Phone Number</label>
+        <input type="tel" className="form-control" id="phoneInput" placeholder="111-111-1111" />
+      </div>
+      <br/>
+      <div class="form-group">
+        <label for="imageInput">Image Url</label>
+        <input type="text" className="form-control" id="imageInput" placeholder="Image Url" />
+      </div>
+      <br/>
+      <button type="button" className="btn btn-primary">Add Contact</button>
+    </form>
   )
 };
 
