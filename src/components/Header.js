1ae65d60ae141bb541ece5e85ac87d3647963af0
Samuel Zimel <samuel.c.zimel@gmail.com>
2022-02-19 13:46:51 -0600

    first- only header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+const Header = () => (
+  <div className="row justify-content-md-center">
+    <div className="col col-md-5 d-flex justify-content-center">
+      <div className="page-header">
+        <h1 className="padding">
+          <b>Contacts</b>
+        </h1>
+      </div>
+    </div>
+  </div>
+);
+
+export default Header;
