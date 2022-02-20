229a9ded154d464cb6574a65c35257f17205ca28
Michael Grumbine <mgrumbine3@gmail.com>
2022-02-19 23:56:19 -0800

    added bootstrap

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const NewContact = () => {};
+
+const Contact = () => {};
+
+const ContactsList = () => {};
+
+const Index = () => (
+  <Routes>
+    <Route exact path="/contacts" component={ContactsList} />
+    <Route path="/contacts/:contact_id" component={Contact} />
+    <Route path="/contacts/new" component={NewContact} />
+  </Routes>
+);
+
+const Homepage = () => <h2>Welcome to the Contact App!</h2>;
+
+const App = () => {
+  const [appData, setAppData] = useState({
+    contacts: [
+      {
+        id: 70219577,
+        name: 'Albert Einstein',
+        image_url:
+          'https://en.wikipedia.org/wiki/Albert_Einstein#/media/File:Einstein_1921_by_F_Schmutzer_-_restoration.jpg',
+        email: 'aeinstein@example.com',
+        phone_number: '15555555555',
+      },
+    ],
+  });
+
+  return (
+    <Routes>
+      <Route path="/" component={Homepage} />
+      <Route path="/contacts" component={Index} />
+    </Routes>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
