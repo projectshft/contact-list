bf150490e840efe7684a073d21d3aa9877d0f78c
Michael Grumbine <mgrumbine3@gmail.com>
2022-02-20 14:35:32 -0800

    added proppTypes to a couple of components, sorting out errors

--- src/index.js
+++ src/index.js
@@ -1,26 +1,41 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const PageNotFound = () => <h1>Sorry, that page does not exist!</h1>;
 
 const NewContact = () => <h1>Create a New Contact</h1>;
 
 const Contact = () => <h1>Contact Page</h1>;
 
-const ContactsList = () => (
-  <div>
-    <h1>All Contacts</h1>
-  </div>
+const ContactsList = ({ contacts }) => (
+  <ul>
+    {contacts.map((contactObj) => (
+      <li>
+        <h2>{contactObj.name}</h2>
+        <img src={contactObj.image_url} alt={`${contactObj.name}'s Portrait`} />
+        <h4>{contactObj.email}</h4>
+        <h5>{contactObj.phone_number}</h5>
+      </li>
+    ))}
+  </ul>
 );
 
-const Index = () => (
-  <Routes>
-    <Route path="/" element={<ContactsList />} />
-    <Route path=":contact_id" element={<Contact />} />
-    <Route path="new" element={<NewContact />} />
-  </Routes>
-);
+const Index = (props) => {
+  const { contacts } = props;
+  return (
+    <Routes>
+      <Route path="/" element={<ContactsList contacts={contacts} />} />
+      <Route path=":contact_id" element={<Contact />} />
+      <Route path="new" element={<NewContact />} />
+    </Routes>
+  );
+};
+
+Index.propTypes = PropTypes.object;
 
 const Homepage = () => (
   <div>
@@ -45,7 +60,11 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
-      <Route path="contacts/*" element={<Index />} />
+      <Route
+        path="contacts/*"
+        element={<Index contacts={appData.contacts} />}
+      />
+      <Route path=":anything_else" element={PageNotFound} />
     </Routes>
   );
 };
