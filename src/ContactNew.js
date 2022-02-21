b7da44ab33b43a807669fdf1bf1717c70a359734
sbistransin <48774173+sbistransin@users.noreply.github.com>
2022-02-20 20:22:25 -0500

    update form button to submit to require inputs

--- src/ContactNew.js
+++ src/ContactNew.js
@@ -29,23 +29,23 @@ const ContactNew = (props) => {
 
     <div className="col-md-5 offset-md-3">
       <div className="row">
-        <form>
+        <form onSubmit={handleSubmitContactClick}>
           <div className="form-group">
             <label htmlFor="nameInput">Full Name</label>
             <input type="text" className="form-control" id="nameInput" placeholder="John Doe" 
-              onChange={event => setName(event.target.value)} />
+              onChange={event => setName(event.target.value)} required/>
           </div>
           <br/>
           <div className="form-group">
             <label htmlFor="emailInput">Email</label>
             <input type="email" className="form-control" id="emailInput" placeholder="John@email.com" 
-              onChange={event => setEmail(event.target.value)} />
+              onChange={event => setEmail(event.target.value)} required/>
           </div>
           <br/>
           <div className="form-group">
             <label htmlFor="phoneInput">Phone Number</label>
             <input type="tel" className="form-control" id="phoneInput" placeholder="111-111-1111" 
-              onChange={event => setPhone(event.target.value)}/>
+              onChange={event => setPhone(event.target.value)} required/>
           </div>
           <br/>
           <div className="form-group">
@@ -54,7 +54,7 @@ const ContactNew = (props) => {
               onChange={event => setImage_url(event.target.value)}/>
           </div>
           <br/>
-          <button type="button" className="btn btn-primary" onClick={handleSubmitContactClick}>Add Contact</button>
+          <button type="submit" className="btn btn-primary">Add Contact</button>
         </form> 
       </div>
     </div>
