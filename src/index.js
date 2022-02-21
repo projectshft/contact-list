2a47bf1c0df0e120e77669bd9462f2800f32a9b9
Doug West <93220603+dougcwest@users.noreply.github.com>
2022-02-21 14:12:15 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,53 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import 'bootstrap/dist/css/bootstrap.css';
-import '../node_modules/font-awesome/css/font-awesome.min.css'
-import './index.css';
-import { BrowserRouter } from 'react-router-dom'
+.page-header {
+  margin-top: 40px;
+  padding-bottom: 10px;
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+.btn {
+  margin-top: 15px;
+}
+
+::placeholder {
+  color: white;
+  font-weight: bold;
+}
+
+.card {
+  border: none;
+}
+
+.card-img-top {
+  width: 100%;
+  height: 15vw;
+  object-fit: cover;
+  border-radius: 50%;
+  padding: 10px;
+}
+
+.card-header {
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+  margin-bottom: 0px;
+}
+
+.hide {
+  margin-bottom: 10px;
+  color: gray;
+  text-decoration: none;
+  text-align: left;
+  margin-left: 0;
+}
+
+.body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+    monospace;
+}
