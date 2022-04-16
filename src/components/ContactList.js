b3232d44d867d696c66025c3ef57250f9698a098
Alex Rodgers <skyewulff@outlook.com>
2022-04-16 16:07:47 -0400

    build and style ContactList template

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,3 +1,5 @@
+import './ContactList.css';
+
 import { ContactsContext } from '../contacts-context';
 
 const ContactList = () => {
@@ -5,13 +7,33 @@ const ContactList = () => {
         <ContactsContext.Consumer>
             {({contacts}) => {
                 return (
-                    <div>
+                    <div className="Contact_List">
                         <h1>Contact List</h1>
-                        <ul>
-                            {contacts.map(contact => {
-                                return <li>{contact.name}</li>
-                            })}
-                        </ul>
+                        <div className="container">
+                        <div className="button-container"><button className="btn btn-primary btn-lg">Add Contact</button></div>
+                            <table className="table table-striped table-hover table-bordered">
+                                <thead className='table-bordered'>
+                                    <tr>
+                                        <th scope="col">Profile Pic</th>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Email</th>
+                                        <th scope="col">Phone Number</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {contacts.map(contact => {
+                                        return (
+                                            <tr>
+                                                <td className="image-cell"><img src={contact.image_url} alt={contact.name} /></td>
+                                                <td>{contact.name}</td>
+                                                <td>{contact.email}</td>
+                                                <td>{contact.phone_number}</td>
+                                            </tr>
+                                        )
+                                    })}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 )
             }}
