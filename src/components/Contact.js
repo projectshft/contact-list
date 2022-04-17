b85f89a9d5e6b459d0038be86c7edbfa00be6672
Alex Rodgers <skyewulff@outlook.com>
2022-04-16 23:15:46 -0400

    build and style Contact template; add logic to route back to contact list

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,13 @@
+import './Contact.css';
+
 import { ContactsContext } from "../contacts-context";
 import { Redirect } from 'react-router-dom';
 
 const Contact = (props) => {
+    const goToContactList = () => {
+        props.history.push('/');
+    }
+
     return (
         <ContactsContext.Consumer>
             {({findContact}) => {
@@ -9,7 +15,17 @@ const Contact = (props) => {
                 
                 if(contact) {
                     return (
-                        <p>{contact.name}</p>
+                        <div className="container">
+                            <h1 onClick={goToContactList}>Contact List</h1>
+                            <div className="card">
+                                <img src={contact.image_url} class="card-img-top" alt={contact.name} />
+                                <div className="card-body">
+                                    <h5 className="card-title">{contact.name}</h5>
+                                    <p className="card-text">{contact.email}</p>
+                                    <p className="card-text">{contact.phone_number}</p>
+                                </div>
+                            </div>
+                        </div>
                     )
                 } else {
                     return <Redirect to="/"/>
