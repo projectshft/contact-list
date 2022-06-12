59bcb89523cb6225cc837e13199c08883855de39
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2022-06-12 16:12:52 -0400

    change for to htmlFor

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -2,7 +2,7 @@ const AddContact = () => {
   return (
     <form>
       <div className="form-group">
-        <label for="full-name">Full Name</label>
+        <label htmlFor="full-name">Full Name</label>
         <input type="text" className="form-control" id="full-name" />
       </div>
       <div className="form-group">
