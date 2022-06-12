4102aef3ff904b1f5b3616b831f0a2e4667fc9fd
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2022-06-12 16:25:57 -0400

    added image label and input

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -4,18 +4,20 @@ const AddContact = () => {
   return (
     <form>
       <div className="form-group">
-        <label htmlFor="full-name">Full Name</label>
+        <label htmlFor="full-name">Full Name:</label>
         <input type="text" className="form-control" id="full-name" />
       </div>
       <div className="form-group">
-        <label htmlFor="email">Email</label>
+        <label htmlFor="email">Email:</label>
         <input type="email" className="form-control" id="email" />
       </div>
       <div className="form-group">
-        <label htmlFor="phone-number">Phone Number</label>
+        <label htmlFor="phone-number">Phone Number:</label>
         <input type="text" className="form-control" id="phone-number" />
       </div>
       <div className="form-group">
+        <label htmlFor="image">Image:</label>
+        <input type="text" className="form-control" id="image" />
       </div>
       <Link to="/">
         <button type="button" className="btn btn-secondary">Go Back</button>
