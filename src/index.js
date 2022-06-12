c3c3f63b259ae52fe9dcc2460bf6a5ed22d48d31
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2022-06-12 15:49:43 -0400

    routes in App.js are working after a long sequence of debuggin and stackoverflow searching

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Router, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Router>
+      <Routes>
         <Route path="/" element={ <App /> }>
         </Route>
-      </Router>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
