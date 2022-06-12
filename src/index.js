734a8ac56803c38dae6f63f2c73727cc23cbc83e
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2022-06-12 14:50:31 -0400

    bootstrap is good to go

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 ReactDOM.render(
   <React.StrictMode>
