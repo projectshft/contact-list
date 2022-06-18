36bf181bd94c18fb19080c336b9b759c0daac786
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-06-18 14:15:39 -0400

    bootstrap works

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <div>Header</div>
+      {/* <Contacts />
+      <AddContacts /> */}
+    </div>
+  );
+}
+
+export default App;
