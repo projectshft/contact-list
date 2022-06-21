33553f29529e25d2205fa2a19d3ca55915373e17
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-06-21 17:41:23 -0400

    added go back button to edit page

--- src/components/EditContact.js
+++ src/components/EditContact.js
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom';
+
+
 const EditContact = () => {
-  return <div>Test Edit Contacts</div>
+  const goBackHandler = (
+    <Link to="/contacts">
+      <button className="btn btn-secondary">Go Back</button>
+    </Link>
+  )
+
+  return (
+    <div>
+      {goBackHandler}
+    </div>
+  )
 }
 
 export default EditContact;
