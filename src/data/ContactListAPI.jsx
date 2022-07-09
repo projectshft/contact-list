f3080df937f30229fc431705adb40b83124d4ae7
drkstn <86159096+drkstn@users.noreply.github.com>
2022-07-09 06:24:52 -0700

    Add initial contact data

---
+++ src/data/ContactListAPI.jsx
@@ -0,0 +1,30 @@
+export default [
+  {
+    id: 1657078494901,
+    image: '/images/6.png',
+    name: 'Dustin Lauffer',
+    email: 'dlauffer@mgmresorts.com',
+    phone: '(702) 927-8188',
+  },
+  {
+    id: 1657078583812,
+    image: '/images/15.png',
+    name: 'Cosmonaut Park',
+    email: 'cosmonautpark@gmail.com',
+    phone: '(775) 873-1239',
+  },
+  {
+    id: 1657078672723,
+    image: '/images/88.png',
+    name: 'darkstone',
+    email: 'darkstone@disroot.org',
+    phone: '(555) 555-5555',
+  },
+  {
+    id: 1657078761634,
+    image: '/images/117.png',
+    name: 'JRNY_NWRD',
+    email: 'jrny_nwrd@gmail.com',
+    phone: '(123) 456-7890',
+  },
+];
