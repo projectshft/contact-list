87cb7ea099062b969c8ef6666bd114baf659c7bf
unknown <dgoodenough@proton.me>
2022-11-14 15:50:54 -0800

    Merge from wesbos linting

--- src/components/ContactId.js
+++ src/components/ContactId.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { Container, Button, Row, Col, Card } from 'react-bootstrap';
 
-const ContactId = ({ contactInfo, setCurrentContact, deleteContact, history }) => {
+const ContactId = ({
+  contactInfo,
+  setCurrentContact,
+  deleteContact,
+  history,
+}) => {
   if (!contactInfo) {
     return (
       <div>
@@ -16,25 +21,41 @@ const ContactId = ({ contactInfo, setCurrentContact, deleteContact, history }) =
   const handleDeleteClick = () => {
     deleteContact(contactInfo.id);
     history.push('/');
-  }
+  };
 
   const handleEditClick = () => {
     history.push(`${contactInfo.id}/edit`);
-  }
+  };
 
   return (
-    <Container className='main-container'>
+    <Container className="main-container">
       <Row>
         <Col xs={12} md={6} className="offset-md-3">
-          <Card bg='secondary' className="contact-card">
-            <Card.Title><h1>{contactInfo.name}</h1></Card.Title>
-            <Card.Img className="profile-img" variant="top" src={contactInfo.imageUrl} />
+          <Card bg="secondary" className="contact-card">
+            <Card.Title>
+              <h1>{contactInfo.name}</h1>
+            </Card.Title>
+            <Card.Img
+              className="profile-img"
+              variant="top"
+              src={contactInfo.imageUrl}
+            />
             <Card.Body>
-              <p><strong>Email:</strong> {contactInfo.email}</p>
-              <p><strong>Phone:</strong> {contactInfo.phone}</p>
+              <p>
+                <strong>Email:</strong> {contactInfo.email}
+              </p>
+              <p>
+                <strong>Phone:</strong> {contactInfo.phone}
+              </p>
               <div>
-                <i onClick={handleDeleteClick} className="fa-solid fa-trash icon"></i>
-                <i onClick={handleEditClick} className="fa-solid fa-file-pen icon"></i>
+                <i
+                  onClick={handleDeleteClick}
+                  className="fa-solid fa-trash icon"
+                />
+                <i
+                  onClick={handleEditClick}
+                  className="fa-solid fa-file-pen icon"
+                />
               </div>
               <Button onClick={() => setCurrentContact(null)}>
                 <Link to="/">Back</Link>
@@ -48,10 +69,16 @@ const ContactId = ({ contactInfo, setCurrentContact, deleteContact, history }) =
 };
 
 ContactId.propTypes = {
-  contacInfo: PropTypes.shape({ name: PropTypes.string, email: PropTypes.string, phone: PropTypes.string, imageUrl: PropTypes.string, id: PropTypes.number }),
+  contactInfo: PropTypes.shape({
+    name: PropTypes.string,
+    email: PropTypes.string,
+    phone: PropTypes.string,
+    imageUrl: PropTypes.string,
+    id: PropTypes.number,
+  }),
   setCurrentContact: PropTypes.func.isRequired,
   deleteContact: PropTypes.func.isRequired,
   history: PropTypes.object.isRequired,
-}
+};
 
 export default ContactId;
