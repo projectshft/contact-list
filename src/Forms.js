1b742bf8989821d4220745d0245593290f78f017
Ezra Godde <ezragodde@Ezras-MacBook-Air.local>
2022-11-18 13:56:57 -0800

    rough outline w/ table

---
+++ src/Forms.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { useState } from 'react';
+
+// function Example() {
+//   // Declare a new state variable, which we'll call "count"
+//   const [name, email, phoneNum, image] = useState(0);
+
+//   return (
+//     <div>
+//       <p>You clicked {count} times</p>
+//       <button onClick={() => setCount(count + 1)}>
+//         Click me
+//       </button>
+//     </div>
+//   );
+// }
+
+
+export default function Forms() {
+  const [name, setName] = useState('Name');
+
+
+  const handleClick = () => {
+  setName('Josh');
+}
+return (
+<form className="post-form">
+<h3>Create A Contact</h3>
+
+<div className="form-group">
+<label for="fullName">Full Name</label>
+  <input type="text" id="search-name" className="form-control" placeholder="Full Name"/>
+
+  <br/>
+  <label for="inputEmail1">Email address</label>
+  <input type="text" id="post-user" className="form-control" placeholder="Email Address"/>
+<br/>
+  <label for="phoneNumber">Phone Number</label>
+  <input type="text" id="post-text" className="form-control" placeholder="Phone Number"/>
+
+<br/>
+<label for="image-url">Image URL</label>
+<input type="url" id="post-user" className="form-control" placeholder="Image URL"/>
+</div>
+<button type="button" className="btn btn-primary add-post" onClick={handleClick}>Add Contact</button>
+          </form>
+);
+}
+
+
+
+// constructor () {
+//   super()
+
+//   this.state = {
+//     user: '',
+//     text: ''
+//   }
+// }
+//example of class need to change to hook
+//useState needs to be import as component
+
+// import React, { useState } from 'react';
+
+// function Example() {
+//   // Declare a new state variable, which we'll call "count"
+//   const [name, email, phoneNum, image] = useState(0);
+
+//   return (
+//     <div>
+//       <p>You clicked {count} times</p>
+//       <button onClick={() => setCount(count + 1)}>
+//         Click me
+//       </button>
+//     </div>
+//   );
+// }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,72 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Forms from './Forms';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+const New = () => (
+  <div>
+    <Forms />
+  </div>
+);
+
+const Contacts = () => (
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">Profile Picture</th>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Phone Number</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+);
+
+const Contact = () => (
+  <h1>Contact</h1>
+);
+
+
+const Main = () => (
+  <main>
+    <Switch>
+      <Route exact path="/contacts/new" component={New} />
+      <Route path="/contacts" component={Contacts} />
+      <Route path="/contact" component={Contact} />
+    </Switch>
+  </main>
+);
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
+    <Main />
     <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
