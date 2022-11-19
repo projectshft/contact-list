66f884ec358dfd721794eb376875900c84bf8913
Carmen Chow <carmendevelops@gmail.com>
2022-11-19 16:37:59 -0500

    add contact form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import ContactList from './components/ContactList';
+import ContactForm from './components/ContactForm';
+
+import {
+	BrowserRouter as Router,
+	Navigate,
+	Route,
+	Routes,
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+			<Router>
+				<Routes>
+					<Route path="/" element={<ContactList/>}/>
+					<Route path="/form" element={<ContactForm/>}/>
+				</Routes>
+			</Router>
     </div>
   );
 }
