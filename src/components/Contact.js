3e4ebb742d874b0964fc582cb34a695bd0fd211a
Daniel Bui <dnlbui@proton.me>
2022-12-06 12:36:20 -0600

    Added contacts skeleton

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+
+
+const Contact = (id) => {
+  //check json for ID then out that contact
+
+
+  return(
+    <h1>Contact</h1>
+  )
+}
+
+export default Contact;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,14 @@
 import { Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import Form from './AddContactForm';
+import Contact from './Contact';
 
 const Main = () => (
   <main>
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route exact path="/form" component={Form} />
+      <Route path="/:id" component={Contact} />
+      <Route path="/form" component={Form} />
     </Switch>
   </main>
 );
