f9e9e15a3e2e6d7f46ddf2b9a947c91f64419595
josephl37 <josephliow2000@gmail.com>
2023-02-19 10:54:09 -0600

    fix spacing issue with table

--- src/ContactList.js
+++ src/ContactList.js
@@ -1,11 +1,21 @@
+import { Link, useHistory } from "react-router-dom";
+
 const ContactList = () => {
+  const history = useHistory();
+
+  const handleRowClick = () => {
+    history.push("/contacts/num");
+  };
+
   return (
     <div>
       <h1 className="text-center mt-5">Contact List</h1>
-      <div className="justify-content-center mx-5" >
-        <a className="btn btn-primary" href="/contacts/new" role="button">
-          Add Contact
-        </a>
+      <div className="align-content-center mx-5 w-75 mx-auto">
+        <Link to="/contacts/new">
+          <a className="btn btn-primary" role="button">
+            Add Contact
+          </a>
+        </Link>
         <table className="table table-hover table-bordered mt-3">
           <thead>
             <tr>
@@ -16,17 +26,24 @@ const ContactList = () => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
+            <tr onClick={handleRowClick}>
+              <td className="">
+                <img
+                  className="mx-auto d-block img-fluid"
+                  src="https://image.shutterstock.com/image-photo/stock-photo-head-shot-portrait-close-up-smiling-confident-businessman-wearing-glasses-looking-at-camera-250nw-1714666150.jpg"
+                />
+              </td>
+              <td className="align-middle">Mark Otto</td>
+              <td className="align-middle">MarkOtto@gmail.com</td>
+              <td className="align-middle">5555555555</td>
             </tr>
             <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
+              <th scope="row" className="text-center">
+                <img />
+              </th>
+              <td className="align-middle">Jacob Thornton</td>
+              <td className="align-middle">jthornton@gmail.com</td>
+              <td className="align-middle">1234567890</td>
             </tr>
           </tbody>
         </table>
