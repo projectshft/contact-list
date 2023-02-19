42d89ab78f3864a587d69e2c34d45eb0054a3d29
josephl37 <josephliow2000@gmail.com>
2023-02-18 23:08:11 -0600

    set up html/css for profile

--- src/Profile.js
+++ src/Profile.js
@@ -1,7 +1,15 @@
 const Profile = () => {
   return (
     <div>
-      <h1>Contact</h1>
+      <h1 className="text-center mt-5">Contact List</h1>
+      <div className="text-center mt-5">
+        <div className="d-inline-block border">
+          <img src='https://image.shutterstock.com/image-photo/stock-photo-head-shot-portrait-close-up-smiling-confident-businessman-wearing-glasses-looking-at-camera-250nw-1714666150.jpg'/>
+          <h4 className="mt-3">Bob Muller</h4>
+          <p className="mt-3">bobmuller@gmail.com</p>
+          <p>5555555555</p>
+        </div>
+      </div>
     </div>
   )
 };
