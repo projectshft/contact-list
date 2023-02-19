4bff41f9536ae915a8d0f066363253a529c82be2
josephl37 <josephliow2000@gmail.com>
2023-02-18 22:20:35 -0600

    added html and css

--- src/ContactList.js
+++ src/ContactList.js
@@ -1,9 +1,38 @@
 const ContactList = () => {
   return (
     <div>
-      <h1 className="text-center">Contact List</h1>
+      <h1 className="text-center mt-5">Contact List</h1>
+      <div className="justify-content-center mx-5" >
+        <a className="btn btn-primary" href="/contacts/new" role="button">
+          Add Contact
+        </a>
+        <table className="table table-hover table-bordered mt-3">
+          <thead>
+            <tr>
+              <th scope="col">Profile Pic</th>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Phone Number</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
+  );
 };
 
-export default ContactList
+export default ContactList;
--- src/ContactNew.js
+++ src/ContactNew.js
@@ -1,9 +1,44 @@
 const ContactNew = () => {
   return (
     <div>
-      <h1>Contact Form</h1>
+      <h1 className="text-center mt-5">Contact List</h1>
+      <form className="col-md-6 offset-md-3">
+        <div class="form-group mt-5">
+          <label>Full Name</label>
+          <input
+            type="text"
+            className="form-control mt-2"
+            placeholder="Enter Full Name"
+          />
+        </div>
+        <div class="form-group mt-3">
+          <label>Email Address</label>
+          <input
+            type="email"
+            className="form-control mt-2"
+            placeholder="Enter Email"
+          />
+        </div>
+        <div class="form-group mt-3">
+          <label>Phone Number</label>
+          <input
+            type="text"
+            className="form-control mt-2"
+            placeholder="Enter Phone"
+          />
+        </div>
+        <div class="form-group mt-3">
+          <label>Image URL</label>
+          <input
+            type="text"
+            className="form-control mt-2"
+            placeholder="Image URL"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary mt-3">Add Contact</button>
+      </form>
     </div>
-  )
+  );
 };
 
-export default ContactNew
+export default ContactNew;
