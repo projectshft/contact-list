ea89a7ce9771843bf6844b4fea040185934d4e5a
josephl37 <josephliow2000@gmail.com>
2023-02-18 23:15:40 -0600

    added back button

--- src/Profile.js
+++ src/Profile.js
@@ -1,17 +1,26 @@
+import { Link } from "react-router-dom";
+
 const Profile = () => {
   return (
     <div>
       <h1 className="text-center mt-5">Contact List</h1>
       <div className="text-center mt-5">
         <div className="d-inline-block border">
-          <img src='https://image.shutterstock.com/image-photo/stock-photo-head-shot-portrait-close-up-smiling-confident-businessman-wearing-glasses-looking-at-camera-250nw-1714666150.jpg'/>
+          <img src="https://image.shutterstock.com/image-photo/stock-photo-head-shot-portrait-close-up-smiling-confident-businessman-wearing-glasses-looking-at-camera-250nw-1714666150.jpg" />
           <h4 className="mt-3">Bob Muller</h4>
           <p className="mt-3">bobmuller@gmail.com</p>
           <p>5555555555</p>
         </div>
       </div>
+      <div className="text-center mt-3">
+        <Link to="/contacts">
+          <a className="btn btn-primary" role="button">
+            Back
+          </a>
+        </Link>
+      </div>
     </div>
-  )
+  );
 };
 
-export default Profile
+export default Profile;
