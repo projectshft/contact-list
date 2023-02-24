42e793e293faa76fe779b3594ca36bc8fc725345
chillyfest <111262007+chillyfest@users.noreply.github.com>
2023-02-24 12:28:47 -0500

    Delete Pages directory

--- Pages/NewContact.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ContactCreate from '../components/ContactCreate.js';
-
-function NewContact({ onSubmit }) {
-    return (
-        <ContactCreate onSubmit={onSubmit} />
-    );
-}
-export default NewContact;
