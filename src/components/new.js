3955d1eab19ed789a958e9b6efecff587d1a54df
cwjohnson7 <cliffj1230@gmail.com>
2023-03-03 14:36:51 -0500

    Initial Commit (w/Route Design)

---
+++ src/components/new.js
@@ -0,0 +1,2 @@
+const NewContact = () => {
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import NewContact from './components/new';
+// import Contact from './components/contact';
+
+
+const App = () => (
+  <div>
+    <Header />
+    <Main />
+  </div>
+);
+
+const ContactsIndex = () => <h1>List of Contacts</h1>;
+
+const Person = () => <h1>Person's Contact Info</h1>;
+
+const NewContact = () => <h1>Add a Contact Here</h1>;
+
+const Header = () => {
+  console.log('Hello World');
+  return (
+    <div className='page-header'>
+      <h1>Contact List</h1>
+    </div>
+  )
+}
+
+const Main = () => (
+  <main>
+    <Switch>
+      <Route exact path="/contacts" component={ContactsIndex} />
+      <Route path="/contacts/person" component={Person} />
+      <Route path="/contacts/new" component={NewContact} />
+    </Switch>
+  </main>
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
