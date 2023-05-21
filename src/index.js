0d72d7a643d51f2a58bfcd0ad052df57989afd40
William Ashley <williemays@Williams-MacBook-Air.local>
2023-05-21 11:49:09 -0400

    inital commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const App = () => (
+  <div>MAIN
+    <Main />
+  </div>
+)
+
+
+const Main = () => (
+  <main>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/contacts" element={<ContactsList />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/contacts/:number" element={<Contact />} />
+    </Routes>
+  </main>
+)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
