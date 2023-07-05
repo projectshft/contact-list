9f0180cde8de025bc5a37567618ba43dc50869a7
Carly Burkhart <21320169+TimeIsOnMySide@users.noreply.github.com>
2023-07-04 20:14:37 -0700

    Starting to make routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <h1>Contact List</h1>
+        <ul>
+          <button>List</button>
+          <button>Add Contact</button>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="contacts" element={<ContactsList />} />
+        <Route path="contacts/new" element={<NewContact />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
