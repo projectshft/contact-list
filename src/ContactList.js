b78d57730cc6692b230a925f63bb7ee418f7af15
etiry <7997357+etiry@users.noreply.github.com>
2023-08-17 16:42:16 -0400

    Center table alignment

--- src/ContactList.js
+++ src/ContactList.js
@@ -15,10 +15,10 @@ const ContactList = ({contacts}) => {
         {contacts.map((contact) => {
           return (
             <tr>
-              <td><img src={contact.image_url} alt="" className="contact-image"/></td>
-              <td>{contact.name}</td>
-              <td>{contact.email}</td>
-              <td>{contact.phone}</td>
+              <td className="text-center"><img src={contact.image_url} alt="" className="contact-image"/></td>
+              <td className="align-middle">{contact.name}</td>
+              <td className="align-middle">{contact.email}</td>
+              <td className="align-middle">{contact.phone}</td>
             </tr>
           )
         })}
