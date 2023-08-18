69699a7b408f774f2089e51b4c4f28d3d4ec46ba
etiry <7997357+etiry@users.noreply.github.com>
2023-08-18 12:13:28 -0400

    Add placeholder new contact form and routing

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,16 @@
-const Header = () => (
-  <div>
+import { useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+
+const Header = () => {
+  const navigate = useNavigate();
+  const handleAddContactClick = () => navigate('/contacts/add');
+
+  return (
+  <div style={{display: 'flex'}}>
     <h1>My Contacts</h1>
+    <Button variant="primary" className="ms-auto" onClick={handleAddContactClick}>Add New Contact</Button>
   </div>
-);
+  )
+};
 
 export default Header;
--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import ContactList from './ContactList';
 import ContactCard from './ContactCard';
+import AddContact from './AddContact';
 
 const Main = ({contacts}) => (
   <main>
     <Routes>
       <Route exact path='/' element={<ContactList contacts={contacts}/>} />
       <Route path='contacts/:id' element={<ContactCard contacts={contacts} />} />
+      <Route path='contacts/add' element={<AddContact />} />
     </Routes>
   </main>
 
