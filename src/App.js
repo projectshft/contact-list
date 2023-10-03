eb29383ea92b791a27943ee20869353ba74ed164
Jordan Cox <jordancox747@outlook.com>
2023-10-03 04:33:54 -0600

    run npm audit fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          Hello
+        </div>
+        <div className="col">
+          World
+        </div>
+      </div>
     </div>
+    </>
   );
 }
 
