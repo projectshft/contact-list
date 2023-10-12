f8404290f8b4a4f953848bd05679b2d48c3f6a9c
Jono Layman <J.layman910@gmail.comgit config --global user.name Jon>
2023-10-12 19:51:47 -0400

    breaking down the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+//  Show the user a list of all of their contacts. "Index page" Make sure it has its own route
+// Add a new contact with a name, image, email and phone number 
+// Use Prototypes
+
+// Routes that will be needed. Index, adding a contact, individual contacts, 
+// We will need a type form for user input to add a contact 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
