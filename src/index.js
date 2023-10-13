045ffc8dc465ff43e047e7e232e5b3e29ca659b9
Jono Layman <J.layman910@gmail.comgit config --global user.name Jon>
2023-10-12 21:10:46 -0400

    added table template

--- src/index.js
+++ src/index.js
@@ -1,14 +1,53 @@
 //  Show the user a list of all of their contacts. "Index page" Make sure it has its own route
-// Add a new contact with a name, image, email and phone number 
+// Add a new contact with a name, image, email and phone number
 // Use Prototypes
 
-// Routes that will be needed. Index, adding a contact, individual contacts, 
-// We will need a type form for user input to add a contact 
+// Routes that will be needed. Index, adding a contact, individual contacts,
+// We will need a type form for user input to add a contact
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-const App = () => <h1>Hello World!</h1>;
+const App = () => (
+  <div>
+    <Index />
+  </div>
+);
+
+const Index = () => (
+  <main>
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td colSpan="2">Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table>
+  </main>
+);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,3 +55,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+export default App;
