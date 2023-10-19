061e87918bb8bf0fefeb2945cdd0b90072b3bad7
Jono Layman <J.layman910@gmail.comgit config --global user.name Jon>
2023-10-19 18:10:03 -0400

    page design and created component files to organize code, submitting new contact not working

---
+++ src/AddContact.Form.js
@@ -0,0 +1,147 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const AddContactForm = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [image, setImage] = useState('');
+  const [contacts, setContacts] = useState([]);
+  const navigate = useNavigate();
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+  }
+  function handleEmailChange(e) {
+    setEmail(e.target.value);
+  }
+  function handlePhoneChange(e) {
+    setPhone(e.target.value);
+  }
+  function handleImageChange(e) {
+    setImage(e.target.value);
+  }
+  function handleAddContact() {
+    const newContact = {
+      name,
+      email,
+      phone,
+      image,
+    };
+
+    setContacts([...contacts, newContact]);
+    setName('');
+    setEmail('');
+    setPhone('');
+    setImage('');
+    navigate('/Index');
+  }
+
+  return (
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            Home
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="/AddContact"
+                >
+                  Add Contact
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/Index">
+                  Index
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="row">
+        <div className="col-md-6 offset-3">
+          <div className="page-header">
+            <h1>Add Contact</h1>
+            <hr />
+          </div>
+          <label htmlFor="Full Name" className="form-label">
+            Full Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="First and Last Name"
+            value={name}
+            onChange={handleNameChange}
+          />
+          <br />
+        </div>
+        <div className="col-md-6 offset-3">
+          <label htmlFor="Email Address" className="form-label">
+            Email Address
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Email Address"
+            value={email}
+            onChange={handleEmailChange}
+          />
+          <br />
+        </div>
+        <div className="col-md-6 offset-3">
+          <label htmlFor="Phone Number" className="form-label">
+            Phone Number
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Phone Number"
+            value={phone}
+            onChange={handlePhoneChange}
+          />
+          <br />
+        </div>
+        <div className="col-md-6 offset-3">
+          <label htmlFor="Image Url" className="form-label">
+            Image URL
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Image URL"
+            value={image}
+            onChange={handleImageChange}
+          />
+          <br />
+        </div>
+      </div>
+      <button
+        type="button"
+        className="btn btn-dark addcontact2-btn"
+        onClick={handleAddContact}
+      >
+        Add Contact
+      </button>
+    </div>
+  );
+};
+
+export default AddContactForm;
--- src/index.js
+++ src/index.js
@@ -1,139 +1,37 @@
-//  Show the user a list of all of their contacts. "Index page" Make sure it has its own route
-// Add a new contact with a name, image, email and phone number
 // Use Prototypes
 
 // Routes that will be needed. Index, adding a contact, individual contacts,
 // The add contact button will route to the add contact page
 // We will need a type form for user input to add a contact
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Outlet, Routes, Route, Link } from 'react-router-dom';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './HomePage';
+import AddContact from './AddContact.Form';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import Index from './AllContacts';
 
-const App = () => (
-  <BrowserRouter>
-    <div>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/Index" element={<Index />} />
-        <Route path="AddContact" element={<AddContact />} />
-      </Routes>
-    </div>
-  </BrowserRouter>
-);
-
-const Home = () => (
-  <div>
-    <h1>Welcome to The Contact List! </h1>
-    <hr />
-    <div />
-    <Link to="/Index">
-      <button type="button" className="btn btn-dark index-btn">
-        Open the Contact Index
-      </button>
-    </Link>
-    <div>
-      <Link to="AddContact">
-        <button type="button" className="btn btn-dark addcontact-btn">
-          Add Contact
-        </button>
-      </Link>
-    </div>
-  </div>
-);
-
-const Index = () => (
-  <main>
-    <h1>Contact List</h1>
-    <hr />
-    <table className="table table-bordered">
-      <thead className="table-dark">
-        <tr>
-          <th scope="col">Avatar</th>
-          <th scope="col">Name</th>
-          <th scope="col">Email</th>
-          <th scope="col">Phone Number</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark Otto</td>
-          <td>Ottoman32@gmail.com</td>
-          <td>5550123</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob Thorton</td>
-          <td>ThorntonJ@gmail.com</td>
-          <td>8675309</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td>Jon Layman</td>
-          <td>LaymanJ@gmail.com</td>
-          <td>1234564</td>
-        </tr>
-      </tbody>
-    </table>
-  </main>
-);
-
-const AddContact = () => (
-  <div className="container">
-    <div className="row">
-      <div className="col-md-6 offset-3">
-        <div className="page-header">
-          <h1>Add Contact</h1>
-          <hr />
-        </div>
-        <label htmlFor="Full Name" className="form-label">
-          Full Name
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="First and Last Name"
-        />
-        <br />
+const App = () => {
+  const [contact, setContacts] = useState([]);
+  const addNewContact = (newContact) => {
+    setContacts([contact, newContact]);
+  };
+  return (
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/Index" element={<Index contact={contact} />} />
+          <Route
+            path="/AddContact"
+            element={<AddContact addNewContact={addNewContact} />}
+          />
+        </Routes>
       </div>
-      <div className="col-md-6 offset-3">
-        <label htmlFor="Email Address" className="form-label">
-          Email Address
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Email Address"
-        />
-        <br />
-      </div>
-      <div className="col-md-6 offset-3">
-        <label htmlFor="Phone Number" className="form-label">
-          Phone Number
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Phone Number"
-        />
-        <br />
-      </div>
-      <div className="col-md-6 offset-3">
-        <label htmlFor="Image Url" className="form-label">
-          Image URL
-        </label>
-        <input type="text" className="form-control" placeholder="Image URL" />
-        <br />
-      </div>
-    </div>
-    <button type="button" className="btn btn-dark addcontact2-btn">
-      Add Contact
-    </button>
-  </div>
-);
+    </BrowserRouter>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
